058497b8d789fac11c9b49518588faa21b5c79c2
nlavanya-tech <nlavanya987@gmail.com>
2020-09-10 07:51:32 +0530

    first commit

---
+++ NotesAppSqlServer.BusinessLayer/Services/NoteService.cs
@@ -0,0 +1,56 @@
+﻿using NotesAppSqlServer.BusinessLayer.Interface;
+using NotesAppSqlServer.BusinessLayer.Services.Repository;
+using NotesAppSqlServer.DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAppSqlServer.BusinessLayer.Services
+{
+    public class NoteService : INoteService
+    {
+        //create local instance 
+        private readonly INoteRepository _repositary;
+
+        public NoteService(INoteRepository repositary)
+        {
+            _repositary = repositary;
+        }
+
+        //private InMemoryDb DbContext;
+        //public NoteService(InMemoryDb DbContext)
+        //{
+        //    this.DbContext = DbContext;
+        //}
+        //Get Notes and retrun list of notes
+        public async Task<IEnumerable<Note>> ReadAsync()
+        {
+            var notes = await _repositary.ReadAsync();
+            // var notes = this.DbContext.notes.ToList();
+            return notes;
+        }
+        //Create notes
+        public async Task<Note> CreateAsync(Note notes)
+        {
+            await _repositary.CreateAsync(notes);
+            //this.DbContext.Add(notes);
+            //this.DbContext.SaveChanges();
+            return notes;
+        }
+        //Update Notes
+        public async Task<Note> UpdateAsync(Note notes)
+        {
+            var note = await _repositary.UpdateAsync(notes);
+
+            return note;
+        }
+        //Delete Notes 
+        public async Task<Note> DeleteAsync(Note notes)
+        {
+            var result = await _repositary.DeleteAsync(notes);
+            return result;
+        }
+    }
+}
---
+++ NotesAppSqlServer/Controllers/NoteController.cs
@@ -0,0 +1,57 @@
+﻿//using NotesAppSqlServer.BusinessLayer.Interface;
+using NotesAppSqlServer.BusinessLayer.Interface;
+using NotesAppSqlServer.DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NotesAppSqlServer.Controllers
+{
+    public class NoteController : ApiController
+    {
+        //create local instance
+        private readonly INoteService Service;
+
+        public NoteController(INoteService service)
+        {
+            this.Service = service;
+        }
+
+        // GET: api/Note
+        //This Method Gets Request Call from User to Read Notes.
+        [HttpGet]
+        public async Task<IEnumerable<Note>> GetAllNotes()
+        {
+            var notes = await this.Service.ReadAsync();
+            return notes;
+        }
+
+        // POST: api/Notes
+        //This Method Gets Request Call from User to Create Notes.
+        [HttpPost]
+        public async void SubmitNotes([FromBody] Note notes)
+        {
+            await this.Service.CreateAsync(notes);
+        }
+
+        // PUT: api/Notes/5
+        //This Method Gets Request Call from User to Update Notes.
+        [HttpPut]
+        public async void UpdateNotes([FromBody] Note notes)
+        {
+            await this.Service.UpdateAsync(notes);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        //This Method Gets Request Call from User to Delete Notes.
+        [HttpDelete]
+        public async void DeleteNotes([FromBody] Note notes)
+        {
+            await this.Service.DeleteAsync(notes);
+        }
+    }
+}
